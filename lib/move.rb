aba30df2d38d05e3c0c01324175a625978cd59bd
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-10 09:52:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   return user_input.to_i - 1
 end
 
-def move(board, user_input, character="X")
-  board[user_input] = character
-  return board
+def move(board, index, char = "X")
+  board[index] = char
+  board
 end
